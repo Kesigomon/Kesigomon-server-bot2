87dab21f5f509698fc989996a59d0de39b282d4d
Kesigomon <kesigomon@gmail.com>
2022-07-25 03:13:55 +0900

    ルール読み終わった後にオーナーがオンラインでないときに案内メッセージを追加した

--- src/cogs/authorize.ts
+++ src/cogs/authorize.ts
@@ -11,12 +11,12 @@ const isAuthorizeable = (member: GuildMember) => {
 };
 
 const authorize = async (members: Array<GuildMember>) => {
-    if (members.length === 0){
+    if (members.length === 0) {
         return
     }
     await Promise.all(members.map((m) => m.roles.add(normalUserRoleId)));
     const channel = await members[0].guild.channels.fetch(joinLogChannelId)
-    if (!(channel instanceof TextChannel)){
+    if (!(channel instanceof TextChannel)) {
         return
     }
     const embed = new MessageEmbed();
@@ -28,22 +28,31 @@ const authorize = async (members: Array<GuildMember>) => {
     await channel.send({embeds: [embed]})
 };
 
-client.on('guildMemberUpdate', async(before, after) => {
-    const isOwner = after.id === after.guild.ownerId;
-    await lock.acquire('authorize', async ()=>{
+client.on('guildMemberUpdate', async (before, after) => {
+    const guild = after.guild;
+    const isOwner = after.id === guild.ownerId;
+    await lock.acquire('authorize', async () => {
         if (
             isOwner
             && before.presence?.status !== after.presence?.status
             && after.presence?.status === 'online'
-        ){
-            await authorize(Array.from(after.guild.members.cache.filter(isAuthorizeable).values()))
-        }
-        else if (!isOwner && isAuthorizeable(after) && before.pending){
-            if ((await after.guild.fetchOwner()).presence?.status === 'online'){
+        ) {
+            await authorize(Array.from(guild.members.cache.filter(isAuthorizeable).values()))
+        } else if (!isOwner && isAuthorizeable(after) && before.pending) {
+            if ((await guild.fetchOwner()).presence?.status === 'online') {
                 await authorize([after]);
-            }
-            else{
-                // Todo: 仮認証完了
+            } else {
+                const channel = await guild.channels.fetch(joinLogChannelId)
+                if (!(channel instanceof TextChannel)) {
+                    return
+                }
+                const embed = new MessageEmbed({
+                    description:
+                        `${memberNicknameMention(after.id)}がルールを読みました。\n
+                        ${memberNicknameMention(guild.ownerId)}がオンラインになると認証されます。\n
+                        オンラインになるまでお待ちください。`
+                });
+                await channel.send({embeds: [embed]})
             }
         }
     })
