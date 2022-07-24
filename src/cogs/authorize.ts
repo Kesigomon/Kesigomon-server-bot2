d70e2a5bf8a4f4550c5160ee2a41b442abc9c7df
Kesigomon <kesigomon@gmail.com>
2022-07-25 03:32:30 +0900

    ステータスの変更はpresenceUpdateらしい

--- src/cogs/authorize.ts
+++ src/cogs/authorize.ts
@@ -5,6 +5,7 @@ import {joinLogChannelId, normalUserRoleId} from '../constant';
 import {memberNicknameMention} from '@discordjs/builders';
 
 const lock = new AsyncLock();
+const AUTHORIZE = 'authorize';
 
 const isAuthorizeable = (member: GuildMember) => {
     return !member.pending && member.roles.cache.size === 1
@@ -30,30 +31,43 @@ const authorize = async (members: Array<GuildMember>) => {
 
 client.on('guildMemberUpdate', async (before, after) => {
     const guild = after.guild;
-    const isOwner = after.id === guild.ownerId;
-    await lock.acquire('authorize', async () => {
-        if (
-            isOwner
-            && before.presence?.status !== after.presence?.status
-            && after.presence?.status === 'online'
-        ) {
-            await authorize(Array.from(guild.members.cache.filter(isAuthorizeable).values()))
-        } else if (!isOwner && isAuthorizeable(after) && before.pending) {
-            if ((await guild.fetchOwner()).presence?.status === 'online') {
-                await authorize([after]);
-            } else {
-                const channel = await guild.channels.fetch(joinLogChannelId)
-                if (!(channel instanceof TextChannel)) {
-                    return
-                }
-                const embed = new MessageEmbed({
-                    description:
-                        `${memberNicknameMention(after.id)}がルールを読みました。
-                        ${memberNicknameMention(guild.ownerId)}がオンラインになると認証されます。
-                        オンラインになるまでお待ちください。`
-                });
-                await channel.send({embeds: [embed]})
+    await lock.acquire(AUTHORIZE, async () => {
+       if (!isAuthorizeable(after) || before.pending) {
+           return
+       }
+        if ((await guild.fetchOwner()).presence?.status === 'online') {
+            await authorize([after]);
+        }
+        else {
+            const channel = await guild.channels.fetch(joinLogChannelId)
+            if (!(channel instanceof TextChannel)) {
+                return
             }
+            const embed = new MessageEmbed({
+                description:
+                    `${memberNicknameMention(after.id)}がルールを読みました。
+                    ${memberNicknameMention(guild.ownerId)}がオンラインになると認証されます。
+                    オンラインになるまでお待ちください。`
+            });
+            await channel.send({embeds: [embed]})
         }
     })
 });
+
+client.on('presenceUpdate', async (before, after)=>{
+    if (
+        !after.member
+        // オーナー以外
+        || after.member.guild.ownerId !== after.member.id
+        // ステータスの変更以外
+        || before?.status === after.status
+        // オンラインでない
+        || after.status !== 'online'
+    ){
+        return
+    }
+    const guild = after.member.guild;
+    await lock.acquire(AUTHORIZE, async () =>{
+        await authorize(Array.from(guild.members.cache.filter(isAuthorizeable).values()));
+    })
+})
